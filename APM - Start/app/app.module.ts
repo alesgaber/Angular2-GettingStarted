5e7388332fc6cef3762c394cb3acb7c364370c0d
alesgaber <ales@tactum.si>
2016-11-27 18:39:22 +0100

    routes

--- APM - Start/app/app.module.ts
+++ APM - Start/app/app.module.ts
@@ -1,13 +1,43 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
-import { AppComponent }  from './app.component';
+import { WelcomeComponent } from './home/welcome.component';
+
+import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
+import { ProductDetailGuard } from './products/product-guard.service';
+import { ProductDetailComponent } from './products/product-detail.component';
+import { ProductFilterPipe } from './products/product-filter.pipe'
+import { StarComponent } from './shared/star.component';
+
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  imports: [ BrowserModule, FormsModule],
-  declarations: [ AppComponent, ProductListComponent ],
-  bootstrap: [ AppComponent ]
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot([
+      { path: 'products', component: ProductListComponent },
+      {
+        path: 'product/:id',
+        canActivate: [ ProductDetailGuard ],
+        component: ProductDetailComponent
+      },
+      { path: 'welcome', component: WelcomeComponent },
+      { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
+    ])
+  ],
+  declarations: [
+    AppComponent,
+    ProductListComponent,
+    ProductFilterPipe,
+    StarComponent,
+    ProductDetailComponent, WelcomeComponent
+  ],
+  providers: [ProductDetailGuard],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
